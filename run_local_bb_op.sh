a45e84a31a9a25ead5199fecb9298ca9b0e5c574
Arvid Paeglit <arvid@deepcode.ai>
2019-12-20 16:04:41 +0100

    released ports

--- run_local_bb_op.sh
+++ run_local_bb_op.sh
@@ -6,7 +6,7 @@ docker run --rm -it \
     -v '/var/run/docker.sock:/var/run/docker.sock' \
     -v $PWD:$PWD \
     -w $PWD \
-    --env HOST_IP --env FRONT_PORT=8080 \
+    --env HOST_IP \
     --env "BIT_BUCKET_SERVER_OAUTH_PRIVATE_KEY=$(cat deepcode.pem)" \
     --env-file ./bb.env \
     --name deepcode \
--- run_local_gl_op.sh
+++ run_local_gl_op.sh
@@ -6,7 +6,7 @@ docker run --rm -it \
     -v '/var/run/docker.sock:/var/run/docker.sock' \
     -v $PWD:$PWD \
     -w $PWD \
-    --env HOST_IP --env FRONT_PORT=8080 \
+    --env HOST_IP \
     --env-file ./gl.env \
     --name deepcode \
     deepcode $@
